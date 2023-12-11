b3e83501ee6053ada4b64045ea0025dfcca4706b
kim kyugnwon <gwon080870@gmail.com>
2023-12-12 02:08:11 +0900

    FEAT: Change UseCase

--- lib/src/features/main/presentation/presenter/main_viewmodel.dart
+++ lib/src/features/main/presentation/presenter/main_viewmodel.dart
@@ -1,13 +1,14 @@
 import 'package:intl/intl.dart';
 import 'package:todo_application/src/features/main/domain/entities/todo_model.dart';
 import 'package:todo_application/src/features/main/domain/repositories/hive_repository.dart';
+import 'package:todo_application/src/features/main/domain/usecases/hive_usecases.dart';
 
 class MainViewModel {
-  final HiveRepository _hiveRepository;
+  final AddTodoUseCase _addTodoUseCase;
 
   const MainViewModel({
-    required HiveRepository hiveRepository,
-  }) : _hiveRepository = hiveRepository;
+    required AddTodoUseCase addTodoUseCase,
+  }) : _addTodoUseCase = addTodoUseCase;
 
   Future<void> addTodo(String title) async {
     Todo todo = Todo(
@@ -16,6 +17,6 @@ class MainViewModel {
       registeredDateTime: DateFormat('yyyy-MM-dd').format(DateTime.now()),
     );
 
-    await _hiveRepository.addTodo(todo);
+    await _addTodoUseCase.addTodo(todo);
   }
 }
