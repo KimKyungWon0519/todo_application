f8d4886c5b4b179faa82d47fa3abd09ce675d074
kim kyugnwon <gwon080870@gmail.com>
2023-12-12 02:37:51 +0900

    FEAT: Add todo

--- lib/src/features/main/presentation/pages/main_page/local_widgets/add_todo_fab.dart
+++ lib/src/features/main/presentation/pages/main_page/local_widgets/add_todo_fab.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:todo_application/src/features/main/presentation/pages/main_page/local_widgets/add_dialog.dart';
+import 'package:todo_application/src/features/main/presentation/presenter/providers.dart';
 
 class AddTodoFAB extends ConsumerWidget {
   const AddTodoFAB({super.key});
@@ -17,6 +18,10 @@ class AddTodoFAB extends ConsumerWidget {
     showDialog(
       context: context,
       builder: (context) => const AddDialog(),
-    );
+    ).then((value) {
+      if (value != null) {
+        ref.read(mainProvider).addTodo(value);
+      }
+    });
   }
 }
