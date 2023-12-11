446c34c721aa2cd523d5fe164dbc406bbe89e73c
kim kyugnwon <gwon080870@gmail.com>
2023-12-12 01:59:36 +0900

    FEAT: Create MainViewModel

--- lib/src/dependency_injection.dart
+++ lib/src/dependency_injection.dart
@@ -1,9 +1,12 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 import 'package:todo_application/src/features/main/data/data_sources/hive_client.dart';
 import 'package:todo_application/src/features/main/data/models/todo_model.dart';
 import 'package:todo_application/src/features/main/data/repositories/hive_repository_impl.dart';
 import 'package:todo_application/src/features/main/domain/repositories/hive_repository.dart';
 import 'package:todo_application/src/features/main/domain/usecases/hive_usecases.dart';
+import 'package:todo_application/src/features/main/presentation/presenter/main_viewmodel.dart';
+import 'package:todo_application/src/features/main/presentation/presenter/providers.dart';
 
 Future initDI() async {
   Box<List<TodoModel>> box = await Hive.openBox('todos');
@@ -26,4 +29,10 @@ Future initDI() async {
       AddTodoUseCase(hiveRepository: hiveRepository);
 
   /* End Initialize UseCase */
+
+  /* Start Initialize ViewModel */
+
+  mainProvider = Provider<MainViewModel>((ref) => MainViewModel());
+
+  /* End Initialize ViewModel */
 }
