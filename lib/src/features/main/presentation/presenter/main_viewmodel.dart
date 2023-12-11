836f7f4ad1a414536433d85b843189fdece76491
kim kyugnwon <gwon080870@gmail.com>
2023-12-12 02:07:01 +0900

    FEAT: Add Todo

--- lib/src/features/main/presentation/presenter/main_viewmodel.dart
+++ lib/src/features/main/presentation/presenter/main_viewmodel.dart
@@ -1,3 +1,5 @@
+import 'package:intl/intl.dart';
+import 'package:todo_application/src/features/main/domain/entities/todo_model.dart';
 import 'package:todo_application/src/features/main/domain/repositories/hive_repository.dart';
 
 class MainViewModel {
@@ -6,4 +8,14 @@ class MainViewModel {
   const MainViewModel({
     required HiveRepository hiveRepository,
   }) : _hiveRepository = hiveRepository;
+
+  Future<void> addTodo(String title) async {
+    Todo todo = Todo(
+      key: -1,
+      title: title,
+      registeredDateTime: DateFormat('yyyy-MM-dd').format(DateTime.now()),
+    );
+
+    await _hiveRepository.addTodos(todo);
+  }
 }
