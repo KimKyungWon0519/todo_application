b3fb152c25f8c35175b7e9a9aa0aa7d44c5033b7
kim kyugnwon <gwon080870@gmail.com>
2023-12-11 23:55:03 +0900

    FEAT: Add Todo Cards

--- lib/src/features/main/presentation/pages/main_page/main_page.dart
+++ lib/src/features/main/presentation/pages/main_page/main_page.dart
@@ -9,14 +9,54 @@ class MainPage extends StatelessWidget {
       appBar: AppBar(
         title: const Text('ToDo'),
       ),
-      body: const Padding(
-        padding: EdgeInsets.all(10),
+      body: Padding(
+        padding: const EdgeInsets.all(10),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            Text('오늘의 목표 : 10개'),
-            Text('미달성 목표 : 10개'),
-            Divider(),
+            const Text('오늘의 목표 : 10개'),
+            const Text('미달성 목표 : 10개'),
+            const Divider(),
+            CheckboxListTile(
+              value: true,
+              onChanged: (value) {},
+              title: Text(
+                '테스트 목표' * 10,
+                overflow: TextOverflow.ellipsis,
+                maxLines: 1,
+                style: const TextStyle(
+                  decoration: TextDecoration.lineThrough,
+                  color: Colors.grey,
+                ),
+              ),
+            ),
+            CheckboxListTile(
+              value: false,
+              onChanged: (value) {},
+              title: Text(
+                '테스트 목표' * 10,
+                overflow: TextOverflow.ellipsis,
+                maxLines: 1,
+                style: const TextStyle(
+                  color: Colors.red,
+                ),
+              ),
+              subtitle: const Text(
+                '등록일(2023/01/01)로 부터 10일 지났어요.',
+                style: TextStyle(
+                  color: Colors.red,
+                ),
+              ),
+            ),
+            CheckboxListTile(
+              value: false,
+              onChanged: (value) {},
+              title: Text(
+                '테스트 목표' * 10,
+                overflow: TextOverflow.ellipsis,
+                maxLines: 1,
+              ),
+            ),
           ],
         ),
       ),
