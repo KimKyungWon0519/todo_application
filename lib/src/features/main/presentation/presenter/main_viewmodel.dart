5971dada977006d5b7fc5e60d15b2294f02fecb1
kim kyugnwon <gwon080870@gmail.com>
2023-12-12 02:26:47 +0900

    FEAT: Update key

--- lib/src/features/main/presentation/presenter/main_viewmodel.dart
+++ lib/src/features/main/presentation/presenter/main_viewmodel.dart
@@ -1,5 +1,5 @@
 import 'package:intl/intl.dart';
-import 'package:todo_application/src/features/main/domain/entities/todo_model.dart';
+import 'package:todo_application/src/features/main/domain/entities/todo.dart';
 import 'package:todo_application/src/features/main/domain/usecases/hive_usecases.dart';
 
 class MainViewModel {
