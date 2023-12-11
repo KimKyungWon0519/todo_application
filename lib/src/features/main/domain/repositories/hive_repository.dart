afb853415c312272f1a809c87339eb14ad65a0e4
kim kyugnwon <gwon080870@gmail.com>
2023-12-12 01:50:17 +0900

    FEAT: Create repo

--- lib/src/features/main/domain/repositories/hive_repository.dart
+++ lib/src/features/main/domain/repositories/hive_repository.dart
@@ -1,1 +1,5 @@
-abstract class HiveRepository {}
+import 'package:todo_application/src/features/main/domain/entities/todo_model.dart';
+
+abstract class HiveRepository {
+  Future<void> addTodos(Todo todo);
+}
