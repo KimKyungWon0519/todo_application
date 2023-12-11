abd4ed43ab9033d5c4d2f47e205af9e689f2a139
kim kyugnwon <gwon080870@gmail.com>
2023-12-12 01:52:05 +0900

    FEAT: Initialize repo

--- lib/src/dependency_injection.dart
+++ lib/src/dependency_injection.dart
@@ -1,9 +1,21 @@
 import 'package:hive_flutter/hive_flutter.dart';
 import 'package:todo_application/src/features/main/data/data_sources/hive_client.dart';
 import 'package:todo_application/src/features/main/data/models/todo_model.dart';
+import 'package:todo_application/src/features/main/data/repositories/hive_repository_impl.dart';
+import 'package:todo_application/src/features/main/domain/repositories/hive_repository.dart';
 
 Future initDI() async {
   Box<List<TodoModel>> box = await Hive.openBox('todos');
 
+  /* Start Initialize Data Source */
+
   HiveClient hiveClient = HiveClient(box: box);
+
+  /* End Initialize Data Source */
+
+  /* Start Initialize Repository */
+
+  HiveRepository hiveRepository = HiveRepositoryImpl(hiveClient: hiveClient);
+
+  /* End Initialize Repository */
 }
