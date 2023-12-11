a463e2b8ab40b5cd25ab9fbe73700368679c1bfc
kim kyugnwon <gwon080870@gmail.com>
2023-12-12 01:59:36 +0900

    FEAT: Create MainViewModel

---
+++ lib/src/features/main/presentation/presenter/providers.dart
@@ -0,0 +1,4 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:todo_application/src/features/main/presentation/presenter/main_viewmodel.dart';
+
+late final Provider<MainViewModel> mainProvider;
