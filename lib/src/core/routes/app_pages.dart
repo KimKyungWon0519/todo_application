f5119763371fc8a4ea607b8747dcffc55629b55f
kim kyugnwon <gwon080870@gmail.com>
2023-12-11 23:06:47 +0900

    FEAT: Add Route

--- lib/src/core/routes/app_pages.dart
+++ lib/src/core/routes/app_pages.dart
@@ -1,3 +1,19 @@
+import 'package:go_router/go_router.dart';
+import 'package:todo_application/src/core/routes/app_routes.dart';
+import 'package:todo_application/src/features/main/presentation/pages/main_page/main_page.dart';
+
 final class AppPages {
   const AppPages._();
+
+  static const String _initializeRoute = AppRoutes.main;
+
+  static final GoRouter router = GoRouter(
+    routes: [
+      GoRoute(
+        path: AppRoutes.main,
+        builder: (context, state) => const MainPage(),
+      ),
+    ],
+    initialLocation: _initializeRoute,
+  );
 }
