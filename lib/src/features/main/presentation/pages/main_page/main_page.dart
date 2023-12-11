3407771b7a6a85275867af70970a2a754d598c31
kim kyugnwon <gwon080870@gmail.com>
2023-12-12 00:42:47 +0900

    FEAT: Create todo tile

--- lib/src/features/main/presentation/pages/main_page/main_page.dart
+++ lib/src/features/main/presentation/pages/main_page/main_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:todo_application/src/features/main/domain/entities/todo_status.dart';
 import 'package:todo_application/src/features/main/presentation/pages/main_page/local_widgets/goal_status.dart';
+import 'package:todo_application/src/features/main/presentation/pages/main_page/local_widgets/todo_tile.dart';
 
 class MainPage extends StatelessWidget {
   const MainPage({super.key});
@@ -18,44 +20,12 @@ class MainPage extends StatelessWidget {
             const AchievedTodo(),
             const NotAchievedTodo(),
             const Divider(),
-            CheckboxListTile(
-              value: true,
-              onChanged: (value) {},
-              title: Text(
-                '테스트 목표' * 10,
-                overflow: TextOverflow.ellipsis,
-                maxLines: 1,
-                style: const TextStyle(
-                  decoration: TextDecoration.lineThrough,
-                  color: Colors.grey,
-                ),
-              ),
-            ),
-            CheckboxListTile(
-              value: false,
-              onChanged: (value) {},
-              title: Text(
-                '테스트 목표' * 10,
-                overflow: TextOverflow.ellipsis,
-                maxLines: 1,
-                style: const TextStyle(
-                  color: Colors.red,
-                ),
-              ),
-              subtitle: const Text(
-                '등록일(2023/01/01)로 부터 10일 지났어요.',
-                style: TextStyle(
-                  color: Colors.red,
-                ),
-              ),
-            ),
-            CheckboxListTile(
-              value: false,
-              onChanged: (value) {},
-              title: Text(
-                '테스트 목표' * 10,
-                overflow: TextOverflow.ellipsis,
-                maxLines: 1,
+            Expanded(
+              child: ListView.builder(
+                itemBuilder: (context, index) {
+                  return TodoTile(todoStatus: TodoStatus.values[index]);
+                },
+                itemCount: TodoStatus.values.length,
               ),
             ),
           ],
