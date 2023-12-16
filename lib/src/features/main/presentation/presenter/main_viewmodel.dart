9b46fa76cefdc40f685c9b1adb669ca965565490
kim kyugnwon <gwon080870@gmail.com>
2023-12-16 17:02:47 +0900

    FEAT: Change providre type ( Provider => StateNotifier )

--- lib/src/features/main/presentation/presenter/main_viewmodel.dart
+++ lib/src/features/main/presentation/presenter/main_viewmodel.dart
@@ -1,17 +1,19 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:intl/intl.dart';
 import 'package:todo_application/src/features/main/domain/entities/todo.dart';
 import 'package:todo_application/src/features/main/domain/entities/todo_status.dart';
 import 'package:todo_application/src/features/main/domain/usecases/hive_usecases.dart';
 
-class MainViewModel {
+class MainViewModel extends StateNotifier<List<Todo>> {
   final AddTodoUseCase _addTodoUseCase;
   final GetTodoUseCase _getTodoUseCase;
 
-  const MainViewModel({
+  MainViewModel({
     required AddTodoUseCase addTodoUseCase,
     required GetTodoUseCase getTodoUseCase,
   })  : _addTodoUseCase = addTodoUseCase,
-        _getTodoUseCase = getTodoUseCase;
+        _getTodoUseCase = getTodoUseCase,
+        super([]);
 
   Future<void> addTodo(String title) async {
     Todo todo = Todo(
