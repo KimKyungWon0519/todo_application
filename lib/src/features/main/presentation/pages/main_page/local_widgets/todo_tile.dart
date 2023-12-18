5781f8a5a41e3970551a896a9d9b829191840a1e
kim kyugnwon <gwon080870@gmail.com>
2023-12-19 00:16:34 +0900

    FEAT: Show remainging time

--- lib/src/features/main/presentation/pages/main_page/local_widgets/todo_tile.dart
+++ lib/src/features/main/presentation/pages/main_page/local_widgets/todo_tile.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:intl/intl.dart';
 import 'package:todo_application/src/features/main/domain/entities/todo.dart';
 import 'package:todo_application/src/features/main/domain/entities/todo_status.dart';
 import 'package:todo_application/src/features/main/presentation/presenter/providers.dart';
@@ -28,14 +29,11 @@ class TodoTile extends ConsumerWidget {
           color: _getColor(),
         ),
       ),
-      subtitle: todo.status == TodoStatus.notAchieved
-          ? Text(
-              '등록일(2023/01/01)로 부터 10일 지났어요.',
-              style: TextStyle(
-                color: _getColor(),
-              ),
-            )
-          : null,
+      subtitle: switch (todo.status) {
+        TodoStatus.achieved => _AchievedSubtitle(todo.registeredDateTime),
+        TodoStatus.notAchieved => const _NotAchievedSubtitle(),
+        _ => null
+      },
     );
   }
 
@@ -56,5 +54,37 @@ class TodoTile extends ConsumerWidget {
     } else {
       ref.read(mainProvider.notifier).unachieveTodo(todo);
     }
+  }
+}
+
+class _AchievedSubtitle extends StatelessWidget {
+  final String registeredDateTime;
+
+  const _AchievedSubtitle(this.registeredDateTime);
+
+  @override
+  Widget build(BuildContext context) {
+    String remainingDate = createRemainingDate();
+
+    return Text('$remainingDate일 후 자동으로 삭제됩니다.');
+  }
+
+  String createRemainingDate() {
+    DateTime dateTime = DateTime.parse(registeredDateTime);
+    int remainingDate = 7 - dateTime.difference(DateTime.now()).inDays.abs();
+
+    return remainingDate.toString();
+  }
+}
+
+class _NotAchievedSubtitle extends StatelessWidget {
+  const _NotAchievedSubtitle();
+
+  @override
+  Widget build(BuildContext context) {
+    return const Text(
+      '등록일(2023/01/01)로 부터 10일 지났어요.',
+      style: TextStyle(color: Colors.red),
+    );
   }
 }
