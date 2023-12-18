084d6892a6883759518fef571cc4702e51c3b5fa
kim kyugnwon <gwon080870@gmail.com>
2023-12-18 22:56:17 +0900

    FEAT: Add achieved status todo

--- lib/src/features/main/domain/repositories/hive_repository.dart
+++ lib/src/features/main/domain/repositories/hive_repository.dart
@@ -3,4 +3,6 @@ import 'package:todo_application/src/features/main/domain/entities/todo.dart';
 abstract class HiveRepository {
   Future<void> addNonStatusTodos(Todo todo);
   List<Todo> getNonStatusTodos();
+
+  Future<void> addAchievedStatusTodo(Todo todo);
 }
